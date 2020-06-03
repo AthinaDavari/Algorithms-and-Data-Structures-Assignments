7dde7b5bd90b729aa4968a065ea2de7de5bc5322
AthinaDavari <t8180020@aueb.gr>
2020-06-03 16:18:39 +0300

    First version of wall handling

--- assignment-2020-3/social_distancing.py
+++ assignment-2020-3/social_distancing.py
@@ -1,5 +1,17 @@
 import math
 import linkedlist
+import random
+import argparse
+
+def read_file(file):
+    space = []
+    with open(file) as graph_input:
+        for line in graph_input:
+            nodes = [float(x) for x in line.split()]
+            if len(nodes) != 4:
+                continue
+            space.append(((nodes[0],nodes[1]),(nodes[2],nodes[3])))
+    return space
 
 def tangent_circle(cm, rm, cn, rn, r):
     dx = cn[0] - cm[0]
@@ -27,7 +39,13 @@ def tangent_line(l, c):
     px = u[0] + t*(v[0] - u[0])
     py = u[1] + t*(v[1] - u[1])
     d = math.sqrt((px - c[0])**2 + ((py - c[1])**2))
-    return d
+    return round(d, 2)
+
+def check_space_limits(space_list, c, r):
+    for l in space_list:
+        if tangent_line(l, c) < r:
+            return True
+    return False
 
 def distance_from_start(center):
     return math.sqrt((center[0])**2 + (center[1])**2)
@@ -53,44 +71,98 @@ def are_tangent_circles(c1, r1, c2, r2):
         return True
     return False
 
+def add_again(dist_from_start, l, listt):
+    prev =  l.find_prev((listt[0])[0])
+    #n = prev.next
+    type = (listt[0])[1]
+    for i in range(1, len(listt)):
+        circle = (listt[-i])[0]
+        l.add(prev, circle)
+        index = (listt[-i])[1]
+        dist = circle[2]
+        if dist in dist_from_start:
+            spec_dist = dist_from_start[dist]
+            spec_dist.insert(index, (circle[0], circle[1]))
+        else:
+            dist_from_start[dist] = [(circle[0], circle[1])]
+        if type == "pr":
+            prev = prev.next
+            #continue
+        #n = n.prev
+
+'''
 def delete_front_nodes(listt, dist_from_start, node1, node2, type):
+    deletad_nodes = []
     if type == "pr":
         flag = True
         middle = node1.prev
+        deletad_nodes.append((node1.value, "pr"))
         while(flag):
             dist = middle.value[2]
             l = dist_from_start[dist]
-            l.remove((middle.value[0], middle.value[1]))
+            index = l.index((middle.value[0], middle.value[1]))
+            del l[index]
+            deletad_nodes.append((middle.value, index))
             if not l:
                 del dist_from_start[dist]
             listt.remove(middle.prev, middle.next)
             middle = middle.prev
             if middle == node2:
                 flag = False
-        return
-    #listt.printLL()
+        return deletad_nodes
+    flag = True
+    middle = node1.next
+    deletad_nodes.append((node2.value, "next"))
+    while(flag):
+        dist = middle.value[2]        
+        l = dist_from_start[dist]
+        index = l.index((middle.value[0], middle.value[1]))
+        del l[index]
+        deletad_nodes.append((middle.value, index))
+        if not l:
+            del dist_from_start[dist]
+        listt.remove(middle.prev, middle.next)
+        middle = middle.next 
+        if middle == node2:
+            flag = False
+    return deletad_nodes
+'''
+
+def delete_front_nodes(listt, dist_from_start, node1, node2, type):
+    deleted_nodes = []
+    if type == "pr":
+        flag = True
+        middle = node1.prev
+        while(flag):
+            dist = middle.value[2]
+            l = dist_from_start[dist]
+            index = l.index((middle.value[0], middle.value[1]))
+            del l[index]
+            deleted_nodes.append((middle.value, index))
+            #l.remove((middle.value[0], middle.value[1]))
+            if not l:
+                del dist_from_start[dist]
+            listt.remove(middle.prev, middle.next)
+            middle = middle.prev
+            if middle == node2:
+                flag = False
+        return deleted_nodes
     flag = True
     middle = node1.next
     while(flag):
         dist = middle.value[2]        
         l = dist_from_start[dist]
-        #print(middle.value)
-        #print("#######")
-        #listt.printLL()
-        l.remove((middle.value[0], middle.value[1]))
+        index = l.index((middle.value[0], middle.value[1]))
+        del l[index]
+        deleted_nodes.append((middle.value, index))
+        #l.remove((middle.value[0], middle.value[1]))
         if not l:
-            #dist_from_start.pop(dist, None)
             del dist_from_start[dist]
-        #print("#######")
-        #listt.printLL()
-        #print(middle.prev.value, middle.value,middle.next.value)
         listt.remove(middle.prev, middle.next)
-        #listt.printLL()
         middle = middle.next 
-        #print(middle.value, node2.value)
-        #print(middle == node2)
         if middle == node2:
             flag = False
+    return deleted_nodes
 
 def circle_tangert_front(l, dist_from_start, pr, n, c, r):
     half_size = math.floor(l.size/2)
@@ -101,94 +173,189 @@ def circle_tangert_front(l, dist_from_start, pr, n, c, r):
     cm = pr.value[0]
     rm = pr.value[1]
     for i in range(half_size):
-        ''' 
-        n = n.next
-        c1 = n.value[0]
-        r1 = n.value[1]
-        if are_tangent_circles(c1 , r1, c, r):
-            #l.printLL()
-            delete_front_nodes(l, dist_from_start, mm, n, "next")
-            #print(n.value, mm.value)
-            return (mm, n)
-        '''
         pr = pr.prev
         c1 = pr.value[0]
         r1 = pr.value[1]
         if are_tangent_circles(c1 , r1, c, r):
-            #l.printLL()
-            delete_front_nodes(l, dist_from_start, nn,  pr, "pr")
-            return (pr, nn)
-        
+            deleted = delete_front_nodes(l, dist_from_start, nn,  pr, "pr")
+            return (nn, pr, deleted)
         n = n.next
         c1 = n.value[0]
         r1 = n.value[1]
         if are_tangent_circles(c1 , r1, c, r):
-            #l.printLL()
-            delete_front_nodes(l, dist_from_start, mm, n, "next")
-            return (mm, n)
-        
+            deleted = delete_front_nodes(l, dist_from_start, mm, n, "next")
+            return (n, mm, deleted)
     if half_size != (l.size/2):
-        '''
-        n = n.next
-        c1 = n.value[0]
-        r1 = n.value[1]
-        if are_tangent_circles(c1 , r1, c, r):
-            #l.printLL()
-            delete_front_nodes(l, dist_from_start, mm, n, "next")
-            return (mm, n)
-        '''
         pr = pr.prev
         c1 = pr.value[0]
         r1 = pr.value[1]
         if are_tangent_circles(c1 , r1, c, r):
-            delete_front_nodes(dist_from_start, nn, pr, "pr")
-            return (nn, pr)
-        
-        #print(mm.value, nn.value)
+            deleted = delete_front_nodes(dist_from_start, nn, pr, "pr")
+            return (pr, nn, deleted)
     return None
 
-r = 10
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--items", type=int, help="number of circles to add in graph")
+parser.add_argument("-r", "--radious", type=int, help="radius of circles")
+parser.add_argument("--min_radious", type=int, help="minimum radius of circles")
+parser.add_argument("--max_radious", type=int, help="maximum radius of circles")
+parser.add_argument("-b", "--bountary_file", action='store', type=str, help="the file with the walls")
+parser.add_argument("-s", "--seed", type=int, default=None, help="seed for random function to create random circles")
+parser.add_argument("ouput_file", action='store', type=str, help="the file with the final circles")
+args = parser.parse_args()
+SEED = args.seed
+MIN_RADIOUS = args.min_radious
+MAX_RADIOUS = args.max_radious
+r = args.radious
+ITEMS = args.items
+space_limit = None
+ouput_file = open(args.ouput_file, "w")
+if args.bountary_file != None:
+    space_limit = read_file(args.bountary_file)
+if SEED != None:
+    random.seed(SEED)
+    rstart= random.randint(MIN_RADIOUS,MAX_RADIOUS)
+else:
+    rstart = r
 start = (0.00, 0.00)
 dist_from_start = {}
-l = linkedlist.Linkedlist()
 c1 = start
-print("{:.2f}".format(c1[0]), "{:.2f}".format(c1[1]), r, sep=" ")
+ouput_file.write("%.2f %.2f %i\n" % (c1[0], c1[1], rstart))
+#print("{:.2f}".format(c1[0]), "{:.2f}".format(c1[1]), rstart, sep=" ")
 sdist = round(distance_from_start(c1), 2)
-add_in_graph(dist_from_start, sdist, (c1, r))
-l.add_last((c1, r, sdist))
-c = (2*r, 0.00)
-print("{:.2f}".format(c[0]), "{:.2f}".format(c[1]), r, sep=" ")
+add_in_graph(dist_from_start, sdist, (c1, rstart))
+l = linkedlist.Linkedlist((c1, rstart, sdist))
+if SEED != None:
+    r= random.randint(MIN_RADIOUS,MAX_RADIOUS)
+c = (rstart+r, 0.00)
+ouput_file.write("%.2f %.2f %i\n" % (c[0], c[1], r))
+#print("{:.2f}".format(c[0]), "{:.2f}".format(c[1]), r, sep=" ")
 sdist = round(distance_from_start(c), 2)
 add_in_graph(dist_from_start, sdist, (c, r))
-l.add_last((c, r, sdist)) 
-for i in range(998):
+l.add_last((c, r, sdist))
+for i in range(ITEMS - 2):     
     flag = True
     m = find_min_dist(dist_from_start)
     prev = l.find_prev(m)
     pr = prev.value
+    if space_limit != None:
+        deleted = []
+        new_list = l.copy()
+        cm = prev.next.value
+    if SEED != None:
+        r= random.randint(MIN_RADIOUS,MAX_RADIOUS)
     while(flag):
-        '''
-        m = find_min_dist(dist_from_start)
-        prev = l.find_prev(m)
-        '''
         pr = prev.value
-        #print(m[0], pr[0])
-        c = tangent_circle(m[0], r, pr[0], r, r)
-        #print("miaw")
-        #print(c)
-        #print("******************")
-        #l.printLL()
-        #print("******************")
+        c = tangent_circle(m[0], m[1], pr[0], pr[1], r)
         ctf = circle_tangert_front(l, dist_from_start, prev, prev.next, c, r)
         if ctf == None:
             flag = False
         else:
-            m = ctf[1].value
-            prev = ctf[0]
-            #print("bourrrrrrrrrr")
-            #print(m, prev.value)
-        #print(flag, " flag")
-    print("{:.2f}".format(c[0]), "{:.2f}".format(c[1]), r, sep=" ")
+            m = ctf[0].value
+            prev = ctf[1]
+            if space_limit != None:
+                #deleted.append(ctf[2])
+                deleted.extend(ctf[2])
+            continue
+        if space_limit != None:
+            limits = check_space_limits(space_limit, c, r)
+            flag = limits
+            if limits:
+                l = new_list.copy()
+                for i in range(len(deleted)-1, -1, -1):
+                    circle = (deleted[i])[0]
+                    index = (deleted[i])[1]
+                    dist = circle[2]
+                    if dist in dist_from_start:
+                        spec_dist = dist_from_start[dist]
+                        spec_dist.insert(index, (circle[0], circle[1]))
+                    else:
+                        dist_from_start[dist] = [(circle[0], circle[1])]
+                prev_cm = l.find_prev(cm)
+                l.remove(prev_cm, prev_cm.next.next)
+                dist = cm[2]
+                listt = dist_from_start[dist]
+                listt.remove((cm[0], cm[1]))
+                if not listt:
+                    del dist_from_start[dist]
+                m = find_min_dist(dist_from_start)
+                prev = l.find_prev(m)
+                pr = prev.value
+                cm = prev.next.value
+                deleted = []
+                new_list = l.copy()
+    ouput_file.write("%.2f %.2f %i\n" % (c[0], c[1], r))
+    #print("{:.2f}".format(c[0]), "{:.2f}".format(c[1]), r, sep=" ")
     sdist = round(distance_from_start(c), 2)
     add_in_graph(dist_from_start, sdist, (c, r))
     l.add(prev, (c ,r , sdist))
+if space_limit:
+    for l in space_limit:
+        ouput_file.write("%.2f %.2f %.2f %.2f\n" % ((l[0])[0], (l[0])[1], (l[1])[0], (l[1])[1]))
+        #print("{:.2f}".format((l[0])[0]), "{:.2f}".format((l[0])[1]), "{:.2f}".format((l[1])[0]), "{:.2f}".format((l[1])[1]), sep=" ")
+ouput_file.close()
+'''
+space_limit = None
+space_limit = []
+space_limit.append(((150, 96.60), (-150, 96.60)))
+space_limit.append(((-150, 96.60), (-150, -96.60)))
+space_limit.append(((-150, -96.60), (150, -96.60)))
+space_limit.append(((150, -96.60), (150, 96.60)))
+SEED = None #13
+MIN_RADIOUS = 5
+MAX_RADIOUS = 10
+r = 10
+ITEMS = 119
+if SEED != None:
+    random.seed(SEED)
+    rstart= random.randint(MIN_RADIOUS,MAX_RADIOUS)
+else:
+    rstart = r
+start = (0.00, 0.00)
+dist_from_start = {}
+l = linkedlist.Linkedlist()
+c1 = start
+print("{:.2f}".format(c1[0]), "{:.2f}".format(c1[1]), rstart, sep=" ")
+sdist = round(distance_from_start(c1), 2)
+add_in_graph(dist_from_start, sdist, (c1, rstart))
+l = linkedlist.Linkedlist((c1, rstart, sdist))
+#l.add_last((c1, rstart, sdist))
+if SEED != None:
+    r= random.randint(MIN_RADIOUS,MAX_RADIOUS)
+c = (rstart+r, 0.00)
+print("{:.2f}".format(c[0]), "{:.2f}".format(c[1]), r, sep=" ")
+sdist = round(distance_from_start(c), 2)
+add_in_graph(dist_from_start, sdist, (c, r))
+l.add_last((c, r, sdist))
+for i in range(ITEMS - 2):
+    flag = True
+    m = find_min_dist(dist_from_start)
+    prev = l.find_prev(m)
+    pr = prev.value
+    if space_limit != None:
+             limits = check_space_limits(space_limit, c, r)
+             flag = limits
+             if limits:
+                 for i in range(len(deleted)-1, -1,-1):
+                     add_again(dist_from_start, l, deleted[i])
+                 prev_cm = l.find_prev(cm)
+                 l.remove(prev_cm, prev_cm.next.next)
+
+                 dist = cm[2]
+                 listt = dist_from_start[dist]
+                 listt.remove((cm[0], cm[1]))
+                 if not listt:
+                     del dist_from_start[dist]
+
+                 #cm = None
+                 m = find_min_dist(dist_from_start)
+                 prev = l.find_prev(m)
+                 pr = prev.value
+                 cm = prev.next.value
+                 deleted = []
+
+    print("{:.2f}".format(c[0]), "{:.2f}".format(c[1]), r, sep=" ")
+    sdist = round(distance_from_start(c), 2)
+    add_in_graph(dist_from_start, sdist, (c, r))
+'''
+
